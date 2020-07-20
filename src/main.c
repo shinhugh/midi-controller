30981d9e556d04c3122795ba91471593074b3b86
Hugh Shin <hughbee135@gmail.com>
2020-07-20 12:12:47 -0700

    Button w/ built-in LED

--- src/main.c
+++ src/main.c
@@ -13,22 +13,25 @@ void main() {
   // Set the 5th bit of DDRB to 1
   DDRB |= (1 << DDB5);
 
+  // Set digital pin 2 to input mode
+  // Set the 2nd bit of DDRD to 0
+  DDRD &= ~(1 << DDD2);
+
   // Loop
   while(1) {
 
-    // Set pin 13 to HIGH
-    // Set the 5th bit of PORTB to 1
-    PORTB |= (1 << PORTB5);
-
-    // Pause
-    _delay_ms(MS_DELAY);
-
-    // Set pin 13 to LOW
-    // Set the 5th bit of PORTB to 0
-    PORTB &= ~(1 << PORTB5);
-
-    // Pause
-    _delay_ms(MS_DELAY);
+    // If digital pin 2 reads a high voltage, turn on LED
+    if((PIND >> PIND2) & 1) {
+      // Set pin 13 to HIGH
+      // Set the 5th bit of PORTB to 1
+      PORTB |= (1 << PORTB5);
+    }
+    // If digital pin 2 reads a low voltage, turn off LED
+    else {
+      // Set pin 13 to LOW
+      // Set the 5th bit of PORTB to 0
+      PORTB &= ~(1 << PORTB5);
+    }
 
   }
 
