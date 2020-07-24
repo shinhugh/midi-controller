52ff36705e256f9fb8432f83d769851fb4ab506a
Hugh Shin <hughbee135@gmail.com>
2020-07-24 13:30:43 -0700

    LED pin info

--- src/main.c
+++ src/main.c
@@ -9,6 +9,9 @@
 
 void main() {
 
+  // ----------------------------------------
+
+  /*
   // Set digital pin 13 to output mode
   // Set the 5th bit of DDRB to 1
   DDRB |= (1 << DDB5);
@@ -34,5 +37,59 @@ void main() {
     }
 
   }
+  */
+
+  // ----------------------------------------
+
+  /*
+   * Liquid crystal LED 16x2 RGB display
+   * - Register select: pin 12 (PB4)
+   * - Enable: pin 11 (PB3)
+   * - Data line d4: pin 10 (PB2)
+   * - Data line d5: pin 9 (PB1)
+   * - Data line d6: pin 8 (PB0)
+   * - Data line d7: pin 7 (PD7)
+   * - Backlight RGB red: pin 6 (PD6)
+   * - Backlight RGB green: pin 5 (PD5)
+   * - Backlight RGB blue: pin 3 (PD3)
+   */
+
+   // Set pin 12 to output mode
+   // Set bit 4 of DDRB to 1
+   DDRB |= (1 << DDB4);
+
+   // Set pin 11 to output mode
+   // Set bit 3 of DDRB to 1
+   DDRB |= (1 << DDB3);
+
+   // Set pin 10 to output mode
+   // Set bit 2 of DDRB to 1
+   DDRB |= (1 << DDB2);
+
+   // Set pin 9 to output mode
+   // Set bit 1 of DDRB to 1
+   DDRB |= (1 << DDB1);
+
+   // Set pin 8 to output mode
+   // Set bit 0 of DDRB to 1
+   DDRB |= (1 << DDB0);
+
+   // Set pin 7 to output mode
+   // Set bit 7 of DDRD to 1
+   DDRD |= (1 << DDD7);
+
+   // Set pin 6 to output mode
+   // Set bit 6 of DDRD to 1
+   DDRD |= (1 << DDD6);
+
+   // Set pin 5 to output mode
+   // Set bit 5 of DDRD to 1
+   DDRD |= (1 << DDD5);
+
+   // Set pin 3 to output mode
+   // Set bit 3 of DDRD to 1
+   DDRD |= (1 << DDD3);
+
+  // ----------------------------------------
 
 }
